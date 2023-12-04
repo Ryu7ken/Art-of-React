e5b758220346c67715bbcae60159019df1a799d6
Ryu7ken <baquiur.rahman@outlook.com>
2023-12-04 23:00:39 +0530

    done

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,12 @@
 import { useRouteError } from "react-router-dom";
+import error from "../components/images/errorpage.png";
 
 const Error = () => {
     const err = useRouteError();
     console.log(err);
     return(
         <div className="w-4/12 m-auto text-center">
-            {/* <img src={errorpage}/> */}
+            <img src={error}/>
             <h1 className="mb-4 font-bold text-3xl">{err.status} : {err.statusText}</h1>
             <h1 className="text-gray-600">
                 Uh-oh! Looks like the page you are trying to access, doesn't exist. Please start afresh.
