cc867457d1e7de0c2478dc33d3f03f29bb89a740
Ryu7ken <baquiur.rahman@outlook.com>
2023-12-15 23:35:07 +0530

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,12 @@ const Header = () => {
     return (
         <div className="h-24 flex justify-between bg-opacity-25 bg-blue-100 shadow-blue-300 shadow-lg">
 
-            {/* <Link to="/">
+            <Link to="/">
                 <div className="ml-10 h-28 scale-105 flex relative overflow-hidden transition duration-300 hover:duration-300 hover:scale-100">
-                    <img src={fplogo} alt="" className="w-20 object-cover bg-transparent " />
-                    <img src={fptext} alt="" className="w-36 ml-2 scale-150 object-cover bg-transparent" />
+                    {/* <img src={fplogo} alt="" className="w-20 object-cover bg-transparent " />
+                    <img src={fptext} alt="" className="w-36 ml-2 scale-150 object-cover bg-transparent" /> */}
                 </div>
-            </Link> */}
+            </Link>
 
             <div className="flex items-center">
                 <ul className="flex my-auto">
