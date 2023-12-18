b98ed5af88591ab665f4046079682fc3de034e57
Ryu7ken <baquiur.rahman@outlook.com>
2023-12-18 15:26:31 +0530

    done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5280352&lng=88.36590830000002&restaurantId=";
+export const MENU_API = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D22.5280352%26lng%3D88.36590830000002%26restaurantId%3D";
 
-export const RES_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5280352&lng=88.36590830000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RES_API = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.5280352%26lng%3D88.36590830000002%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 
 export const OFFER_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/";
 
