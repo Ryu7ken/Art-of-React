eaa24411fd2016c974c03f14756dae7ceae10a0b
Ryu7ken <baquiur.rahman@outlook.com>
2024-01-12 15:56:40 +0530

    done

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,14 +6,13 @@ const RestaurantCard = (props) => {
     const {resData} = props;
     const {name, cuisines, avgRating, costForTwo, cloudinaryImageId, areaName} = resData?.info;
     const {deliveryTime} = resData?.info?.sla;
-    const {header, subHeader} = resData?.info?.aggregatedDiscountInfoV3;
 
     return (
         <div data-testid="resCard" className="my-6 w-72 scale-95 bg-gray-100 rounded-2xl shadow-lg shadow-blue-300 transition duration-300 hover:duration-300 hover:scale-100">
 
             <div className="relative h-52 w-72 rounded-2xl overflow-hidden">
                 <img className="object-cover w-full h-full" alt="res-image" src={CDN_URL + cloudinaryImageId} />
-                <h1 className="absolute w-8/12 mb-2 pl-2 bottom-0 inset-x-0 text-white text-sm text-left font-bold bg-blue-500">{header} {subHeader}</h1>
+                <h1 className="absolute w-8/12 mb-2 pl-2 bottom-0 inset-x-0 text-white text-sm text-left font-bold bg-blue-500">₹120 OFF ABOVE ₹199</h1>
             </div>
 
             <h3 className="pl-3 py-2 font-bold text-lg truncate">{name}</h3>
